56e9634653a4b029b39ca3d19ec8eba678140d08
Dan Flanigan <DFlanigan@revspringinc.com>
2021-06-17 10:20:29 -0500

    Adjusted help message

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -9,7 +9,7 @@ namespace WiredBrainCoffee.ShopInfoTool
         private static void Main(string[] args)
         {
             Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
-            Console.WriteLine("Type 'help' to list available commands");
+            Console.WriteLine("Type 'help' to list available coffee shop commands");
 
             var coffeeShopDataProvider = new CoffeeShopDataProvider();
             var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops().ToList();
