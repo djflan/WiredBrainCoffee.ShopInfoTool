f427266c4b3a091f5462158eb9f9c98de40938af
Dan Flanigan <DFlanigan@revspringinc.com>
2021-06-17 09:31:48 -0500

    Add project files.

---
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WiredBrainCoffee.ShopInfoTool
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+
+            Console.ReadKey();
+        }
+    }
+}
