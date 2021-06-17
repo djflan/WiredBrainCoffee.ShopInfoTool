1e556dcaa36f864d72cdd936bec24df8d2c8aed5
Dan Flanigan <DFlanigan@revspringinc.com>
2021-06-17 10:09:49 -0500

    Added data access project to solution

---
+++ WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using WiredBrainCoffee.DataAccess.Model;
+
+namespace WiredBrainCoffee.DataAccess
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop {Location = "Nashville", BeansInStockInKg = 107};
+            yield return new CoffeeShop {Location = "Murfreesboro", BeansInStockInKg = 23};
+            yield return new CoffeeShop {Location = "Clarksville", BeansInStockInKg = 56};
+        }
+    }
+}
---
+++ WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
@@ -0,0 +1,11 @@
+﻿namespace WiredBrainCoffee.DataAccess.Model
+{
+    public class CoffeeShop
+    {
+        public string Location { get; set; }
+
+        public double BeansInStockInKg { get; set; }
+
+        public double BeansInStockInLb => BeansInStockInKg / 2.2;
+    }
+}
