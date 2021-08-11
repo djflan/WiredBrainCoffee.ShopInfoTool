b5012890a9d13ef80445e4ae84d2d46e087b94a4
Dan Flanigan <DFlanigan@revspringinc.com>
2021-08-11 11:38:52 -0500

    Implemented command handlers

---
+++ WiredBrainCoffee.ShopInfoTool/CoffeeShopCommandHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WiredBrainCoffee.DataAccess.Model;
+
+namespace WiredBrainCoffee.ShopInfoTool
+{
+    internal class CoffeeShopCommandHandler : ICommandHandler
+    {
+        private readonly List<CoffeeShop> _coffeeShops;
+        private readonly string _line;
+
+        public CoffeeShopCommandHandler(List<CoffeeShop> coffeeShops, string line)
+        {
+            _coffeeShops = coffeeShops;
+            _line = line;
+        }
+
+        public void HandleCommands()
+        {
+            var matchingShops = _coffeeShops.Where(cs =>
+                cs.Location.StartsWith(_line, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            if (!matchingShops.Any())
+            {
+                Console.WriteLine($"> Invalid command '{_line}'");
+            }
+
+            if (matchingShops.Count == 1)
+            {
+                var matchedShop = matchingShops.First();
+
+                Console.Out.WriteLine($"> Location: {matchedShop.Location}.\n" +
+                                      $"> Beans in stock: {matchedShop.BeansInStockInKg} kg.\n" +
+                                      $"> Paper cups in stock: {matchedShop.PaperCupsInStock}.");
+            }
+
+            if (matchingShops.Count > 1)
+            {
+                Console.Out.WriteLine($"> Multiple locations found for {_line}:");
+
+                foreach (var matchingShop in matchingShops)
+                {
+                    Console.Out.WriteLine($"> {matchingShop.Location}");
+                }
+            }
+        }
+    }
+}
---
+++ WiredBrainCoffee.ShopInfoTool/HelpCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using WiredBrainCoffee.DataAccess.Model;
+
+namespace WiredBrainCoffee.ShopInfoTool
+{
+    internal class HelpCommandHandler : ICommandHandler
+    {
+        private readonly List<CoffeeShop> _coffeeShops;
+
+        public HelpCommandHandler(List<CoffeeShop> coffeeShops)
+        {
+            _coffeeShops = coffeeShops;
+        }
+
+        public void HandleCommands()
+        {
+            Console.WriteLine("> Available coffee shop commands:");
+            foreach (var shop in _coffeeShops)
+            {
+                Console.WriteLine($"> {shop.Location}");
+            }
+        }
+    }
+}
